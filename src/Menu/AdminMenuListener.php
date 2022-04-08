b66472d74d6058b6023f8d52750137949bd9546c
Jérémy COSTA <39689570+Costa-Jeremy@users.noreply.github.com>
2022-04-08 15:14:30 +0200

    Create AdminMenuListener.php

---
+++ src/Menu/AdminMenuListener.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace  Arobases\SyliusRightsManagementPlugin\Menu;
+
+use Sylius\Bundle\UiBundle\Menu\Event\MenuBuilderEvent;
+
+final class AdminMenuListener
+{
+    public function addAdminMenuItems(MenuBuilderEvent $event): void
+    {
+        $menu = $event->getMenu();
+        $menu->getChild('configuration')->addChild('roles',[
+            'route' => 'arobases_sylius_rights_management_plugin_admin_role_index'
+        ])->setLabel('arobases_sylius_rights_management_plugin.menu.roles')->setLabelAttribute('icon', 'users');
+    }
+}
