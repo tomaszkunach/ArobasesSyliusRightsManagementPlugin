15bbcf47a45325501aeaefe745c93e235c12dffb
Jérémy COSTA <39689570+Costa-Jeremy@users.noreply.github.com>
2022-04-12 15:15:03 +0200

    Update AdminUserTypeExtension.php

--- src/Form/Extension/AdminUserTypeExtension.php
+++ src/Form/Extension/AdminUserTypeExtension.php
@@ -24,7 +24,7 @@ class AdminUserTypeExtension extends AbstractTypeExtension
 //            ])
 //        ;
         $builder->add('role', RoleChoiceType::class, [
-            'label' => 'arobases.sylius.rights_management_plugin.ui.role',
+            'label' => 'arobases_sylius_rights_management_plugin.ui.role',
             'required' => false,
             'multiple' => false,
             'expanded' => true,
